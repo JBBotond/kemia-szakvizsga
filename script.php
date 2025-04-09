18d7b2c263dbe70bb1aeea7ecbcdfb9979d63261
JBBotond <167197141+JBBotond@users.noreply.github.com>
2025-04-09 14:52:53 +0300

    maga a php file

--- script.php
+++ script.php
@@ -3,8 +3,18 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
             //adat
-            
-            $row="lefutott";
+            $cSzam = $_POST['CSzam'];
+            $hSzam = $_POST['HSzam'];
+
+            //$sql = "SELECT CH FROM `kemia` WHERE CONCAT(\"C1\",\"H4\") = CH;";
+            $row[0] = $hSzam;
+            $row[1] = $cSzam;
+            $sqlCH = "SELECT CH FROM `kemia` WHERE CONCAT('C', '$cSzam','H', '$hSzam') = CH;";
+            $sqlNev = "SELECT nev FROM `kemia` WHERE CONCAT('C', '$cSzam','H', '$hSzam') = CH;";
+            $table = mysqli_query($a,$sqlCH);
+            $row[0] = mysqli_fetch_array($table);
+            $table = mysqli_query($a,$sqlNev);
+            $row[1] = mysqli_fetch_array($table);
             header("Content-Type: application/json"); // JSON fejléc beállítása
             echo json_encode($row, JSON_PRETTY_PRINT); // JSON küldése
     }
